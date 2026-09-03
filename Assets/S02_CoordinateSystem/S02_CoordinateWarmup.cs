5d18ca1990917fb69df994818449e08288c8e338
Ha DaSom <2371068@ewhain.net>
2026-09-03 11:38:13 +0900

    Initial commit

---
+++ Assets/S02_CoordinateSystem/S02_CoordinateWarmup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class S02_CoordinateWarmup : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private Vector3 targetRotationEuler;
+    [SerializeField] private Vector3 targetScale = Vector3.one; //[SerializeField] : inspector에 노출
+
+    void Update()
+    {
+        transform.position = targetPosition;
+        transform.rotation = Quaternion.Euler(targetRotationEuler);
+        transform.localScale = targetScale; //transform : gameobject의 transform object. <-> 대문자 Transfrom
+    }
+}
